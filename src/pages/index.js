d9556cf0e3eb733d4a17c1d98e34d71b2e476395
sandipMakerStudio <sandipkarmokar@makerstudio.io>
2024-04-22 13:36:44 +0530

    added preline

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,31 @@
 import * as React from "react";
 import { useState } from "react";
+import { Script } from "gatsby";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Accordion from "../components/accordion";
+
+import "preline/preline";
+import { IStaticMethods } from "preline/preline";
+window.HSStaticMethods = IStaticMethods;
 
 const IndexPage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.HSStaticMethods.autoInit();
+  }, [location.pathname]);
+
   const [list, setList] = useState(["apple", "orange", "mango", "tomato"]);
   return (
-    <div class="h-screen bg-red-300">
-      <p class="text-4xl font-bold text-red-500 p-12">
-        This is a test to learn about gatsby
-      </p>
-    </div>
+    <>
+      <div class="h-screen bg-red-300">
+        <Accordion></Accordion>
+      </div>
+      {/* <Script src="./node_modules/preline/dist/preline.js" /> */}
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/preline/preline.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 };
